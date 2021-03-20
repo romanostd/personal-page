5e128cedf78378dfb37e45f3f7ed6e1c157aea44
Romano M. Stedile <54461411+romanostd@users.noreply.github.com>
2021-03-20 16:42:41 -0300

    Atualizado informações

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-/*!
-    * Start Bootstrap - Freelancer v6.0.4 (https://startbootstrap.com/themes/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-    */
-    (function($) {
+(function($) {
     "use strict"; // Start of use strict
   
     // Smooth scrolling using jQuery easing
